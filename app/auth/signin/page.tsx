1fc23ac11f4a1d476f613c312630eeb096aa7fbe
Jacki <jacquit.aja@onja.org>
2025-08-28 10:59:27 +0300

    Start creating the navbar

---
+++ app/auth/signin/page.tsx
@@ -0,0 +1,5 @@
+export default function signinPage() {
+    // Implement your sign-in logic here
+    // For example, you might handle form submission or redirect to an auth provider
+    return null;
+}
